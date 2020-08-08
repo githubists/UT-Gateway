5bedf5e030c534bd9674f0b5d8059c0b17166fc6
sunrise <thoftbank@gmail.com>
2020-08-08 23:17:09 +0900

    ログアウト機能の実装

--- src/content.js
+++ src/content.js
@@ -1,14 +1,26 @@
 chrome.runtime.onMessage.addListener((message) => {
-  var index = location.pathname.lastIndexOf("/");
-  var former = location.pathname.substr(0, index + 1);
-  var latter = location.pathname.substr(index + 1);
+  if (location.hostname == "gateway2.itc.u-tokyo.ac.jp") {
+    fetch("https://gateway2.itc.u-tokyo.ac.jp/dana-na/auth/logout.cgi");
 
-  location.href =
-    "https://gateway2.itc.u-tokyo.ac.jp" +
-    former +
-    ",DanaInfo=" +
-    location.hostname +
-    ",SSL+" +
-    latter +
-    location.search;
+    var former_path = location.pathname.split("/")[1];
+    var origin = location.pathname.split("/")[2].split(",")[1].split("=")[1];
+    var latter_path = location.pathname.split("/")[2].split(",")[2].substr(4);
+    var search = location.search;
+
+    location.href =
+      "https://" + origin + "/" + former_path + "/" + latter_path + search;
+  } else {
+    var index = location.pathname.lastIndexOf("/");
+    var former = location.pathname.substr(0, index + 1);
+    var latter = location.pathname.substr(index + 1);
+
+    location.href =
+      "https://gateway2.itc.u-tokyo.ac.jp" +
+      former +
+      ",DanaInfo=" +
+      location.hostname +
+      ",SSL+" +
+      latter +
+      location.search;
+  }
 });
